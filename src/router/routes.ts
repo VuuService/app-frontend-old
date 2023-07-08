e1635ca30fc9d42db71f67bbc2df4dfa11724e11
AR-Life <ayunus155@gmail.com>
2023-07-08 18:44:09 +0300

    new route add routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,26 @@
 import { RouteRecordRaw } from 'vue-router'
-import { RouterName } from '@/enums/RouteName'
+import { RouteName } from '@/enums/RouteName'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    name: RouterName.home,
+    name: RouteName.home,
     component: () => import('@/layouts/MainLayout.vue')
+  },
+  {
+    path: '/account',
+    name: RouteName.account,
+    component: () => import('@/views/account/AccountIndex.vue')
+  },
+  {
+    path: '/customers',
+    name: RouteName.customers,
+    component: () => import('@/views/account/AccountIndex.vue')
+  },
+  {
+    path: '/users',
+    name: RouteName.users,
+    component: () => import('@/views/users/UsersIndex.vue')
   }
 ]
 export default routes
