b6dee30dfae59290e5131cca5d8d92bae5b1ad20
AR-Life <ayunus155@gmail.com>
2023-07-08 18:41:57 +0300

    AccountIndex created for User Profile And Setting

---
+++ src/views/account/AccountIndex.vue
@@ -0,0 +1,95 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="w-full max-w-sm bg-white">
+    <div class="flex justify-end px-4 pt-4">
+      <button
+        id="dropdownButton"
+        class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+        data-dropdown-toggle="dropdown"
+        type="button"
+      >
+        <span class="sr-only">Open dropdown</span>
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 16 3"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"
+          />
+        </svg>
+      </button>
+      <!-- Dropdown menu -->
+      <div
+        id="dropdown"
+        class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+      >
+        <ul aria-labelledby="dropdownButton" class="py-2">
+          <li>
+            <a
+              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              href="#"
+              >Edit</a
+            >
+          </li>
+          <li>
+            <a
+              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              href="#"
+              >Export Data</a
+            >
+          </li>
+          <li>
+            <a
+              class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+              href="#"
+              >Delete</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex flex-col items-center pb-10">
+      <img
+        alt="Bonnie image"
+        class="w-24 h-24 mb-3 rounded-full shadow-lg"
+        src="https://picsum.photos/200"
+      />
+      <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Osman Piker</h5>
+      <span class="text-sm text-gray-500 dark:text-gray-400">İşyeri Sahibi</span>
+    </div>
+    <div class="grid grid-cols-2 gap-2 px-2">
+      <a
+        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        href="#"
+      >
+        <i class="vuu-account-outline text-2xl"></i>
+        <h5 class="font-bold tracking-tight text-gray-900 dark:text-white">İşletme</h5></a
+      >
+      <a
+        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        href="#"
+      >
+        <i class="vuu-account-outline text-2xl"></i>
+        <h5 class="font-bold tracking-tight text-gray-900 dark:text-white">Personel</h5>
+      </a>
+      <a
+        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        href="#"
+      >
+        <i class="vuu-account-outline text-2xl"></i>
+        <h5 class="font-bold tracking-tight text-gray-900 dark:text-white">Ürün</h5>
+      </a>
+      <a
+        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        href="#"
+      >
+        <i class="vuu-account-outline text-2xl"></i>
+        <h5 class="font-bold tracking-tight text-gray-900 dark:text-white">Kasa</h5>
+      </a>
+    </div>
+  </div>
+</template>
