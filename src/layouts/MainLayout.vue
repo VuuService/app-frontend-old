2b8b26dc8eefd92270eed1f43ba53930aab3df0a
AR-Life <ayunus155@gmail.com>
2023-07-08 18:43:24 +0300

    Main Layout Created

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import BottomNavigation from '@/layouts/BottomNavigation.vue'
+import NavBar from '@/layouts/NavBar.vue'
+</script>
+<template>
+  <NavBar></NavBar>
+  <div class="pt-16 mt-2 px-2">
+    <router-view></router-view>
+  </div>
+
+  <BottomNavigation></BottomNavigation>
+</template>
