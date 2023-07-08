7b40e151a3a4e907ada287f3826155afd942ef03
AR-Life <ayunus155@gmail.com>
2023-07-08 18:42:56 +0300

    BottomNavigation created

---
+++ src/layouts/BottomNavigation.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { RouteName } from '@/enums/RouteName'
+
+interface menuItemInterface {
+  icon: string
+  name: string
+  url: string
+}
+
+const menuItems = ref<menuItemInterface[]>([
+  { icon: 'vuu-home', name: 'Anasayfa', url: RouteName.home },
+  { icon: 'vuu-account-group', name: 'Müşteriler', url: RouteName.customers },
+  { icon: 'vuu-account', name: 'Hesabım', url: RouteName.account }
+])
+</script>
+<template>
+  <div
+    class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600 md:hidden"
+  >
+    <div class="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
+      <div v-for="menuItem in menuItems" :key="menuItem.icon">
+        <router-link
+          :to="{ name: menuItem.url }"
+          class="inline-flex text-center flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          type="button"
+        >
+          <i
+            :class="menuItem.icon"
+            class="text-3xl text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+          ></i>
+          <span
+            class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            >{{ menuItem.name }}</span
+          >
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
