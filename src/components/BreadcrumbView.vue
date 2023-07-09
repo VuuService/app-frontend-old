4f99964225737034e188cc9988ed0c97df0fee54
AR-Life <ayunus155@gmail.com>
2023-07-09 17:18:43 +0300

    Breadcrumb component created

---
+++ src/components/BreadcrumbView.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav aria-label="Breadcrumb" class="flex p-2 shadow">
+    <ol class="inline-flex items-center space-x-1 md:space-x-3">
+      <li class="inline-flex items-center">
+        <a
+          class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+          href="#"
+        >
+          <i class="vuu-account"></i>
+          Hesabım
+        </a>
+      </li>
+      <li aria-current="page">
+        <div class="flex items-center">
+          <svg
+            aria-hidden="true"
+            class="w-3 h-3 text-gray-400 mx-1"
+            fill="none"
+            viewBox="0 0 6 10"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m1 9 4-4-4-4"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+            />
+          </svg>
+          <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400"
+            >Personel</span
+          >
+        </div>
+      </li>
+    </ol>
+  </nav>
+</template>
