9df5dc00135fd49d1b1d94333121fdd55211348d
AR-Life <ayunus155@gmail.com>
2023-07-15 13:09:44 +0300

    AccountIndex.vue max-width to w-full

--- src/views/account/AccountIndex.vue
+++ src/views/account/AccountIndex.vue
@@ -1,7 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { RouteName } from '@/enums/RouteName'
+</script>
 
 <template>
-  <div class="w-full max-w-sm bg-white">
+  <div class="w-full bg-white">
     <div class="flex justify-end px-4 pt-4">
       <button
         id="dropdownButton"
@@ -63,28 +65,30 @@
     </div>
     <div class="grid grid-cols-2 gap-2 px-2">
       <a
-        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        class="inline-flex items-center justify-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         href="#"
       >
         <i class="vuu-account-outline text-2xl"></i>
-        <h5 class="font-bold tracking-tight text-gray-900 dark:text-white">İşletme</h5></a
+        <h5 class="font-bold tracking-tight text-gray-900 text-center dark:text-white">
+          İşletme
+        </h5></a
       >
-      <a
-        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-        href="#"
+      <router-link
+        :to="{ name: RouteName.users }"
+        class="inline-flex items-center justify-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
         <i class="vuu-account-outline text-2xl"></i>
         <h5 class="font-bold tracking-tight text-gray-900 dark:text-white">Personel</h5>
-      </a>
+      </router-link>
       <a
-        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        class="inline-flex items-center justify-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         href="#"
       >
         <i class="vuu-account-outline text-2xl"></i>
         <h5 class="font-bold tracking-tight text-gray-900 dark:text-white">Ürün</h5>
       </a>
       <a
-        class="inline-flex items-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        class="inline-flex items-center justify-center p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         href="#"
       >
         <i class="vuu-account-outline text-2xl"></i>
