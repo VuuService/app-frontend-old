9b0869790b8de8db02cffbe62dd1d2e7d49a2cb1
AR-Life <ayunus155@gmail.com>
2023-07-29 05:38:13 +0300

    InputView.vue changed

--- src/components/InputView.vue
+++ src/components/InputView.vue
@@ -2,8 +2,15 @@
   <div class="relative z-0 w-full mt-4 group">
     <input
       :id="id"
+      v-model="model"
+      :disabled="disabled"
+      :max="max"
+      :pattern="pattern"
       :required="required"
+      :step="type == 'number' ? '0.001' : ''"
+      :title="title"
       :type="type"
+      :value="value || modelValue"
       class="block pt-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
       placeholder=" "
     />
@@ -15,7 +22,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { randomInt } from '@/utils/math'
 
 const id = ref<string>('input' + randomInt(1, 9999 * 9999))
@@ -24,11 +31,27 @@ export interface Props {
   placeholder?: string
   type?: string
   required?: boolean
+  modelValue?: any
+  value?: any
+  disabled?: boolean
+  max?: number
+  pattern?: string
+  title?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
   placeholder: 'hello',
   type: 'text',
   required: false
+})
+const emit = defineEmits(['update:modelValue'])
+
+const model = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    return emit('update:modelValue', value)
+  }
 })
 </script>
