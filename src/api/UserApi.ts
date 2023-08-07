8325c13f280735b1d9ffb4cb2e3dc0cb59f88cc3
AR-Life <ayunus155@gmail.com>
2023-08-07 07:29:22 +0300

    UserApi add isRole function

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,4 +1,5 @@
 import { userStore } from '@/stores/AuthStore'
+import type { CompanyInterface } from '@/api/CompanyApi'
 
 export interface UserInterface {
   firstName: string
@@ -12,10 +13,15 @@ export interface UserInterface {
     access_token: string
     refresh_token: string
   }
+  company: CompanyInterface
 }
 
-const { permissions } = userStore()
+const { permissions, user } = userStore()
 
 export function isGranted(permission: string) {
   return permissions.includes(permission)
+}
+
+export function isRole(role: string[]) {
+  return role.includes(user.role)
 }
