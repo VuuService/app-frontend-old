012db74f9051790b159ba1410797e14c2d6db0b6
AR-Life <ayunus155@gmail.com>
2023-08-08 12:59:00 +0300

    session-modal added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 import Main from '@/layouts/MainLayout.vue'
 import { userStore } from '@/stores/AuthStore'
+import SessionModal from '@/components/SessionModal.vue'
 
-const { isAuth } = userStore()
+const store = userStore()
 </script>
 <template>
-  <Main v-if="isAuth"></Main>
+  <Main v-if="store.isAuth"></Main>
   <router-view v-else></router-view>
+  <teleport to="body">
+    <session-modal v-if="store.isAuth"></session-modal>
+  </teleport>
 </template>
