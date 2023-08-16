73bbd8d8728a2138af5c645123c9f0c359efe336
AR-Life <ayunus155@gmail.com>
2023-08-16 10:59:28 +0300

    CustomersEdit.vue created

---
+++ src/views/customers/CustomersEdit.vue
@@ -0,0 +1,76 @@
+<template>
+  <breadcrumb-view></breadcrumb-view>
+  <form class="p-4" @submit.prevent="submit">
+    <input-view v-model="customer.firstName" placeholder="İsim"></input-view>
+    <input-view v-model="customer.lastName" placeholder="Soyisim"></input-view>
+    <input-view
+      v-model="customer.phone"
+      pattern="[0-9]{10}"
+      placeholder="Telefon Numarası"
+      title="Başında sıfır olmadan girin."
+      type="tel"
+    ></input-view>
+    <cities-view v-if="customer.address" v-model="customer.address"></cities-view>
+    <definitions-panel
+      v-model="customer.properties"
+      :module="ModuleName.users"
+      :properties="customer.properties"
+    ></definitions-panel>
+    <!-- //TODO  Burada konum butonuna tıklanınca openstreetmap adres bilgisi çekeceğiz. ayrıca geolocalation.coords -->
+    <!-- //TODO input masklar yapılacak. quasara benzer bakıcaz buna -->
+    <!-- //TODO  Burada sadece müşteri kaydetmek istene bilir. Eğer ki müşteriye cihaz satmak isteniyorsa ya da müşterinin cihazına direk bir bakım yapılacaksa kaydetin dışında butonlar koymalıyız. bu butonlar önce müşteriyi kaydedecek daha sonra ilgili ekrana atacak. Ekranları kaydırmalı düşünmüştüm. Ancak kullanıcının kafası karışa bilir. müşteri kayıt edildikten sonra ilgili ekranı route yaparak yönlendirmek en doğrusu hem müşterinin bilgileri gösterilir.
+
+     -->
+    <div class="grid grid-cols-2 gap-2 py-4">
+      <button
+        class="inline-flex items-center justify-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        type="button"
+      >
+        <i class="vuu-home"></i>
+        <span> Satış İşlemleri </span>
+      </button>
+      <button
+        class="inline-flex items-center justify-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        type="button"
+      >
+        <i class="vuu-home"></i>
+        <span> Bakım İşlemleri </span>
+      </button>
+    </div>
+    <button
+      class="w-full text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      type="submit"
+    >
+      Kaydet
+    </button>
+  </form>
+</template>
+<script lang="ts" setup>
+import BreadcrumbView from '@/components/BreadcrumbView.vue'
+import InputView from '@/components/InputView.vue'
+import { defineAsyncComponent, onMounted, ref } from 'vue'
+import type { CustomerInterface } from '@/api/CustomersApi'
+import { customerData, getCustomer, updateCustomer } from '@/api/CustomersApi'
+import CitiesView from '@/components/CitiesView.vue'
+import { ModuleName } from '@/enums/ModuleName'
+import { userStore } from '@/stores/AuthStore'
+import { useRoute } from 'vue-router'
+
+const DefinitionsPanel = defineAsyncComponent({
+  loader: () => import('@/views/definitions/DefinitionsPanel.vue'),
+  delay: 200
+})
+
+const customer = ref<CustomerInterface>({ ...customerData })
+const user = userStore()
+const submit = async () => {
+  customer.value.company = user.company
+  await updateCustomer(customer.value).then((r) => console.log(r))
+}
+const route = useRoute()
+onMounted(async () => {
+  if (route.params?.id) {
+    customer.value = await getCustomer(route.params.id)
+  }
+})
+</script>
