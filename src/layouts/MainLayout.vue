4433eb0496c87108ff5fbf29052e2cea86511fbd
AR-Life <ayunus155@gmail.com>
2023-08-18 14:18:28 +0300

    desktop view settings

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,14 +3,16 @@ import BottomNavigation from '@/layouts/BottomNavigation.vue'
 import NavBar from '@/layouts/NavBar.vue'
 </script>
 <template>
-  <NavBar></NavBar>
-  <router-view v-slot="{ Component, route }">
-    <transition mode="out-in" name="fade">
-      <div :key="route.path" class="py-16 mt-2">
-        <component :is="Component"></component>
-      </div>
-    </transition>
-  </router-view>
+  <div class="max-w-md mx-auto max-h-lg">
+    <NavBar></NavBar>
+    <router-view v-slot="{ Component, route }">
+      <transition mode="out-in" name="fade">
+        <div :key="route.path" class="py-16 mt-2">
+          <component :is="Component"></component>
+        </div>
+      </transition>
+    </router-view>
 
-  <BottomNavigation></BottomNavigation>
+    <BottomNavigation></BottomNavigation>
+  </div>
 </template>
