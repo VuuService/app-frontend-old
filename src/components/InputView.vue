98d1fda0f2dbc9be6a9860fc0fb9311005c3a711
AR-Life <ayunus155@gmail.com>
2023-08-19 22:40:42 +0300

    add valid and invalid css

--- src/components/InputView.vue
+++ src/components/InputView.vue
@@ -11,7 +11,7 @@
       :title="title"
       :type="type"
       :value="value || modelValue"
-      class="block pt-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+      class="invalid:border-red-600 valid:border-green-600 block pt-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
       placeholder=" "
     />
     <label
