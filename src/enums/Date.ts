f311de9ac192c524ced4f7c918d288271494ac96
AR-Life <ayunus155@gmail.com>
2023-08-26 11:37:33 +0300

    add Date enum

---
+++ src/enums/Date.ts
@@ -0,0 +1,10 @@
+export enum timeType {
+  millisecond = 'millisecond',
+  second = 'second',
+  minute = 'minute',
+  hour = 'hour',
+  day = 'day',
+  week = 'week',
+  month = 'month',
+  year = 'year'
+}
