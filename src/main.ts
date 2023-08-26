6919168115db25b3578d9f33b10a2652470bb41f
AR-Life <ayunus155@gmail.com>
2023-08-26 11:37:58 +0300

    app use i18n

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { i18n } from '@/i18n/i18n'
 
 const app = createApp(App)
 
@@ -13,5 +14,5 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
-
+app.use(i18n)
 app.mount('#app')
